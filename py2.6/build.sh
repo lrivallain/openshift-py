0501c27624889b62d000440d8d28f485d467810c
Ludovic Rivallain <ludovic.rivallain@gmail.com>
2014-09-05 14:44:15 +0200

    Fixes #1 : Complete the list of environment variables

--- py2.6/build.sh
+++ py2.6/build.sh
@@ -7,14 +7,14 @@
 ###############################################################################
 
 # build image
-docker build --rm -q -t lrivallain/openshift-py2.7 .
+docker build --rm -q -t lrivallain/openshift-py2.6 .
 
 # create a shared folder
 boot2docker ssh "sudo [ -d /data/openshift ] || sudo mkdir -p /data/openshift"
 boot2docker ssh "sudo chown -R docker: /data/openshift"
 
 # Copy files to shared folder
-scp -q -i ~/.ssh/id_boot2docker -r ./ docker@boot2docker:/data/openshift
+scp -q -i ~/.ssh/id_boot2docker -r ../ docker@boot2docker:/data/openshift
 
 # Run Openshift container
-docker run --rm -v /data/openshift:/data -p 8080:8080 -ti lrivallain/openshift-py2.7
+docker run --rm -v /data/openshift:/data -p 8080:8080 -ti lrivallain/openshift-py2.6
--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import sys
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "<p>Hello World!</p><p><em>Running on Python %s</em></p>" % sys.version
 
 if __name__ == "__main__":
     app.run()
